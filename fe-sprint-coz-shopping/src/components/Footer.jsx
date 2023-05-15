144028aa058ff8ea2de1165944d3b25a7f70282b
dongbin420 <dongbin420@gmail.com>
2023-05-15 12:17:28 +0900

    footer 완성

---
+++ fe-sprint-coz-shopping/src/components/Footer.jsx
@@ -0,0 +1,27 @@
+import styled from "styled-components";
+
+import React from "react";
+
+export const FooterContainer = styled.div`
+  width: 100%;
+  height: 6%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  border-top: 0.5px solid lightgray;
+  color: lightgray;
+  font-size: 0.75em;
+  font-weight: 300;
+`;
+
+const Footer = () => {
+  return (
+    <FooterContainer>
+      <div>개인정보 처리방침 | 이용 약관</div>
+      <div>All rights reserved @ Codestates</div>
+    </FooterContainer>
+  );
+};
+
+export default Footer;
---
+++ fe-sprint-coz-shopping/src/components/Header.jsx
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+import React from "react";
+
+export const HeaderContainer = styled.div`
+  width: 100%;
+  height: 9%;
+`;
+
+const Header = () => {
+  return <HeaderContainer></HeaderContainer>;
+};
+
+export default Header;
